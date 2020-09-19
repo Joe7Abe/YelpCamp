43d116c7bdcd2e0b675290023c9165bc41117527
jabraham97 <jabraham97@gmail.com>
2020-09-19 18:06:54 +0000

    All files

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+	res.render("landing");
+});
+
+app.get("/campgrounds", function(req,res){
+	res.render("landing");
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+	console.log("YelpCamp server has started!");
+})
